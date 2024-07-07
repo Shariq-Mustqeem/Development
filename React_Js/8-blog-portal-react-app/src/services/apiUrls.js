27ea32642b675fe9e490dd069c003c1176e01414
Shariq-Mustaqeem <shariqmustaqeem22@gmail.com>
2024-07-07 17:03:00 +0500

    Working

--- React_Js/8-blog-portal-react-app/src/services/apiUrls.js
+++
@@ -1,9 +0,0 @@
-export const API_URLS = {
-  GET_USERS: "/users",
-  GET_CATEGORY: "/categories",
-  GET_POST: "/posts",
-  GET_SEARCH: "/search",
-  GET_COMMENTS: "/comments",
-  REGISTER: "/register",
-  LOGIN: "/login",
-};
--- React_Js/8-blog-portal-react-app/src/services/search.service.js
+++
@@ -1,8 +0,0 @@
-import { apiService } from "../utils/api.service";
-import { API_URLS } from "./apiUrls";
-
-const serachPost = (data) => apiService.post(API_URLS.GET_SEARCH, data);
-
-export const SerachService = {
-  serachPost,
-};
